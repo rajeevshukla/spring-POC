7878b078a0f1a251ea74d05ff4dc785368a5b555
rajeevshukla <mail2rajeevshukla@gmail.com>
2016-08-25 17:41:42 +0530

    configuration related changes

---
+++ SpringSecurityV1.0/src/com/devstringx/pmt/utils/ApplicationMaps.java
@@ -0,0 +1,8 @@
+package com.devstringx.pmt.utils;
+
+import java.util.Properties;
+
+public class ApplicationMaps {
+public static 	Properties PMT_PROPERTIES=new Properties();
+	
+}
---
+++ SpringSecurityV3.0/src/com/devstringx/pmt/web/admin/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.devstringx.pmt.web.admin;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WelcomeController {
+
+	
+	
+	public void bootLoader(){
+		System.out.println("this method is called.");
+	}
+}
