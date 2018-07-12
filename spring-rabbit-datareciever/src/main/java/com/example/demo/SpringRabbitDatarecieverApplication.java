3f0756bd1295610655819319a55b566ed80e4c60
n9952335 <n9952335@ncclt7ip35kg562.windstream.com>
2018-07-11 23:07:28 -0400

    Formatting.

--- spring-rabbit-datareciever/src/main/java/com/example/demo/SpringRabbitDatarecieverApplication.java
+++ spring-rabbit-datareciever/src/main/java/com/example/demo/SpringRabbitDatarecieverApplication.java
@@ -8,5 +8,6 @@ public class SpringRabbitDatarecieverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitDatarecieverApplication.class, args);
-	}
+	} 
+	
 }
